e437a9038a2345d8939b36f4d67c65f91fc8478a
ayanonishikawa <yaanon1116@gmail.com>
2022-05-06 16:13:32 +0900

    Initial commit

--- W06/task1_main.js
+++ W06/task1_main.js
@@ -51,8 +51,14 @@ class ScatterPlot {
         self.xaxis = d3.axisBottom( self.xscale )
             .ticks(6);
 
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(6);
+        
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`);
+        
+        self.yaxis_group = self.chart.append('g')
+            .attr('transform', `translate(${self.inner_width}, 0)`);
     }
 
     update() {
@@ -82,5 +88,7 @@ class ScatterPlot {
 
         self.xaxis_group
             .call( self.xaxis );
+        self.yaxis_group
+            .call( self.yaxis );
     }
 }
