00bbfbeae611718eb1acd14b9cb152f7fbf3a443
ayanonishikawa <yaanon1116@gmail.com>
2022-05-06 16:18:27 +0900

    Initial commit

--- W06/task1_main.js
+++ W06/task1_main.js
@@ -58,7 +58,7 @@ class ScatterPlot {
             .attr('transform', `translate(0, ${self.inner_height})`);
         
         self.yaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, 0)`);
+            .attr('transform', `translate(${self.inner_width}, 0)`);
     }
 
     update() {
