8ecf414e5a3d0251908a2ff7b13dd4ddba7f7933
ayanonishikawa <yaanon1116@gmail.com>
2022-05-09 18:37:12 +0900

    Initial commit

--- W06/task2_main.js
+++ W06/task2_main.js
@@ -58,11 +58,18 @@ class ScatterPlot {
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`)
             .append("text")
+            .attr("x", self.config.margin.left)
+            .attr("y", inner_height)
+            .attr("text-anchor", "middle")
             .text("X_label");
         
         self.yaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, 0)`)
             .append("text")
+            .attr("x", 0)
+            .attr("y", inner_height/2)
+            .attr("transform", "rotate(-90)")
+            .attr("text-anchor", "middle")
             .text("Y_label");
     }
 
