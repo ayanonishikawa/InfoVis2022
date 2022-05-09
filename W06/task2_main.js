d0e0f088d4e6d4b5ddada0de795a83f339502f34
ayanonishikawa <yaanon1116@gmail.com>
2022-05-10 00:54:36 +0900

    Initial commit

--- W06/task2_main.js
+++ W06/task2_main.js
@@ -106,20 +106,20 @@ class ScatterPlot {
             .attr("r", d => d.r);
 
         self.xaxis_group
-            .call(self.xaxis)
-            .append("text")
-            .attr("x", self.config.margin.left)
-            .attr("y", self.inner_height)
-            .attr("text-anchor", "middle")
-            .text("X_label");
+            .call(self.xaxis);
+            // .append("text")
+            // .attr("x", self.config.margin.left)
+            // .attr("y", self.inner_height)
+            // .attr("text-anchor", "middle")
+            // .text("X_label");
 
         self.yaxis_group
-            .call(self.yaxis)
-            .append("text")
-            .attr("x", 0)
-            .attr("y", self.inner_height / 2)
-            .attr("transform", "rotate(-90)")
-            .attr("text-anchor", "middle")
-            .text("Y_label");
+            .call(self.yaxis);
+            // .append("text")
+            // .attr("x", 0)
+            // .attr("y", self.inner_height / 2)
+            // .attr("transform", "rotate(-90)")
+            // .attr("text-anchor", "middle")
+            // .text("Y_label");
     }
 }
