af8ffb785f94cdca0bc88eb8fc0071533ccde9ec
ayanonishikawa <yaanon1116@gmail.com>
2022-05-09 15:29:17 +0900

    Initial commit

--- W06/task1_main.js
+++ W06/task1_main.js
@@ -1,12 +1,13 @@
 d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/data.csv")
     .then( data => {
         data.forEach( d => { d.x = +d.x; d.y = +d.y; });
-        console.log("ok3");
+        console.log("ok4");
         var config = {
             parent: '#drawing_region',
             width: 256,
             height: 256,
-            margin: {top:10, right:10, bottom:20, left:40}
+            margin: {top:10, right:10, bottom:20, left:40},
+            axisMargin: {top: 10,right: 10, bottom: 10, left: 10}
         };
 
         const scatter_plot = new ScatterPlot( config, data );
@@ -23,7 +24,8 @@ class ScatterPlot {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+            margin: config.margin || {top:10, right:10, bottom:10, left:10},
+            axisMargin: config.axisMargin || {top: 10,right: 10, bottom: 10, left: 10}
         }
         this.data = data;
         this.init();
@@ -40,13 +42,16 @@ class ScatterPlot {
             .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.axis_inner_width=self.config.width - self.config.margin.left - self.config.margin.right-self.config.axisMargin.left-self.config.axisMargin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+        self.axis_inner_height=self.config.height  - self.config.margin.top - self.config.margin.bottom-self.config.axisMargin.top-self.config.axisMargin.bottom;
+    
 
         self.xscale = d3.scaleLinear()
-            .range( [0, self.inner_width] );
+            .range( [0, self.axis_inner_width] );
 
         self.yscale = d3.scaleLinear()
-            .range( [self.inner_height, 0] );
+            .range( [self.axis_inner_height, 0] );
 
         self.xaxis = d3.axisBottom( self.xscale )
             .ticks(6);
