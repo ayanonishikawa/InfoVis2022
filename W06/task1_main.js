4c9e3b775bafdfe0e65155bdbd97ee211f44dd6e
ayanonishikawa <yaanon1116@gmail.com>
2022-05-09 23:50:45 +0900

    Initial commit

--- W06/task1_main.js
+++ W06/task1_main.js
@@ -8,16 +8,13 @@ d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W06/w06_task1.csv")
             height: 256,
             margin: {top:10, right:10, bottom:40, left:40},
         };
-
         const scatter_plot = new ScatterPlot( config, data );
         scatter_plot.update();
     })
     .catch( error => {
         console.log( error );
     });
-
 class ScatterPlot {
-
     constructor( config, data ) {
         this.config = {
             parent: config.parent,
@@ -28,24 +25,18 @@ class ScatterPlot {
         this.data = data;
         this.init();
     }
-
     init() {
         let self = this;
-
         self.svg = d3.select( self.config.parent )
             .attr('width', self.config.width)
             .attr('height', self.config.height);
-
         self.chart = self.svg.append('g')
-            .attr("transform","translate(${self.config.margin.left}, ${self.config.margin.top})");
-
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
         console.log(self.inner_width+","+self.inner_height);
-
         self.xscale = d3.scaleLinear()
             .range( [0, self.inner_width] );
-
         self.yscale = d3.scaleLinear()
             .range( [self.inner_height, 0] );
 
@@ -56,38 +47,30 @@ class ScatterPlot {
         self.yaxis = d3.axisLeft( self.yscale )
             .ticks(10)
             .tickPadding(2);
-        
+
         self.xaxis_group = self.chart.append('g')
-            .attr("transform", "translate(0, ${self.inner_height})");
+            .attr('transform', `translate(0, ${self.inner_height})`);
         
         self.yaxis_group = self.chart.append('g')
-            .attr("transform", "translate(0, 0)");
+            .attr('transform', `translate(0, 0)`);
     }
-
     update() {
         let self = this;
-
         const xmin = 0;
         const xmax = d3.max( self.data, d => d.x );
         //self.xscale.domain( [xmin, xmax+20] );
-
         const ymin = 0;
         const ymax = d3.max( self.data, d => d.y );
         //self.yscale.domain( [ymin, ymax+20] );
-
         var larger=0;
         if(xmin>xmax) larger=xmin;
         else larger=xmax;
-
         self.xscale.domain( [xmin, larger+20] );
         self.yscale.domain( [ymin, larger+20] );
-
         self.render();
     }
-
     render() {
         let self = this;
-
         self.chart.selectAll("circle")
             .data(self.data)
             .enter()
@@ -95,7 +78,6 @@ class ScatterPlot {
             .attr("cx", d => self.xscale( d.x ) )
             .attr("cy", d => self.yscale( d.y ) )
             .attr("r", d => d.r );
-
         self.xaxis_group
             .call( self.xaxis );
         self.yaxis_group
