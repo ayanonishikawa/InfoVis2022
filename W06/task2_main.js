cff0cd28797867ed08af7bce43600bc94fabd88f
ayanonishikawa <yaanon1116@gmail.com>
2022-05-10 17:46:08 +0900

    Initial commit

--- W06/task2_main.js
+++ W06/task2_main.js
@@ -1,7 +1,7 @@
 d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W06/w06_task1.csv")
     .then( data => {
         data.forEach( d => { d.x = +d.x; d.y = +d.y; });
-        console.log("ok15");
+        console.log("ok16");
         var config = {
             parent: '#drawing_region',
             width: 400,
@@ -81,7 +81,7 @@ class ScatterPlot {
         self.chart
             .append("text")
             .attr("x", self.inner_height/2)
-            .attr("y", -20)
+            .attr("y", -40)
             .attr("transform", "rotate(-90)")
             .attr("text-anchor", "middle")
             .attr("font-size","10pt")
