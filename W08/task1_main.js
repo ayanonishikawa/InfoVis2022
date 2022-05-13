295dc092d82dcda494ac10bf0bca8348de640062
ayanonishikawa <yaanon1116@gmail.com>
2022-05-14 00:51:25 +0900

    Initial commit

--- W08/task1_main.js
+++ W08/task1_main.js
@@ -60,7 +60,7 @@ class ScatterPlot {
 
         // Draw the axis
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`);
+            .attr('transform', `translate(0, ${self.inner_height+10})`);
 
         self.yaxis_group = self.chart.append('g');
     }
