d66420c8b41e106f3338134111a124f7bfcba06e
ayanonishikawa <yaanon1116@gmail.com>
2022-05-16 22:48:56 +0900

    Initial commit

--- W08/task3_main.js
+++ W08/task3_main.js
@@ -1,13 +1,16 @@
 d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W04/vitaminC_ranking.csv")
     .then(data => {
-        data.forEach(d => { d.label = d.name; d.value = +d.amount; 
-        console.log(d.label+","+d.value)});
+        data.forEach(d => {
+            d.label = d.name; d.value = +d.amount;
+            console.log(d.label + "," + d.value)
+        });
         console.log("ok6");
         var config = {
             parent: '#drawing_region',
-            width: 500,
+            width: 300,
             height: 300,
-            margin: { top: 50, right: 10, bottom: 70, left: 120},
+            margin: { top: 50, right: 10, bottom: 70, left: 120 },
+            radius: Math.min( width, height ) / 2
         };
         const scatter_plot = new ScatterPlot(config, data);
         scatter_plot.update();
@@ -22,7 +25,8 @@ class ScatterPlot {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || { top: 10, right: 10, bottom: 10, left: 10 }
+            margin: config.margin || { top: 10, right: 10, bottom: 10, left: 10 },
+            radius: config.radius|| 150
         }
         this.data = data;
         this.init();
@@ -35,61 +39,68 @@ class ScatterPlot {
             .attr('height', self.config.height);
 
         self.chart = self.svg.append('g')
-            .attr('transform', `translate(${width/2}, ${height/2})`);
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
         console.log(self.inner_width + "," + self.inner_height);
 
-        // Initialize axis scales
-        self.xscale = d3.scaleLinear()
-            .range([0, self.inner_width]);
+        self.pie = d3.pie()
+            .value(d => d.value);
 
-        self.yscale = d3.scaleBand()
-            .range([0, self.inner_height])
-            .paddingInner(0.1);
+        self.arc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(self.config.radius);
 
-        // Initialize axes
-        self.xaxis = d3.axisBottom(self.xscale)
-            .ticks(5)
-            .tickSizeOuter(0);
+        // // Initialize axis scales
+        // self.xscale = d3.scaleLinear()
+        //     .range([0, self.inner_width]);
 
-        self.yaxis = d3.axisLeft(self.yscale)
-            .tickSizeOuter(0);
+        // self.yscale = d3.scaleBand()
+        //     .range([0, self.inner_height])
+        //     .paddingInner(0.1);
 
-        // Draw the axis
-        self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`);
+        // // Initialize axes
+        // self.xaxis = d3.axisBottom(self.xscale)
+        //     .ticks(5)
+        //     .tickSizeOuter(0);
 
-        self.yaxis_group = self.chart.append('g');
+        // self.yaxis = d3.axisLeft(self.yscale)
+        //     .tickSizeOuter(0);
 
-        self.chart
-            .append("text")
-            .attr("x", self.config.margin.left)
-            .attr("y", -10)
-            .attr("text-anchor", "middle")
-            .text("Vitamin C content of vegetables")
-            .attr("font-size","12pt")
-            .attr("font-weight", "bold")
-            .style("textDecoration","underline");
+        // // Draw the axis
+        // self.xaxis_group = self.chart.append('g')
+        //     .attr('transform', `translate(0, ${self.inner_height})`);
 
-        self.chart
-            .append("text")
-            .attr("x", self.inner_width/2)
-            .attr("y", self.inner_height+self.config.margin.bottom/2)
-            .attr("text-anchor", "middle")
-            .attr("font-size","8pt")
-            .attr("font-weight", "bold")
-            .text("Vitamine C mg/100g");
+        // self.yaxis_group = self.chart.append('g');
+
+        // self.chart
+        //     .append("text")
+        //     .attr("x", self.config.margin.left)
+        //     .attr("y", -10)
+        //     .attr("text-anchor", "middle")
+        //     .text("Vitamin C content of vegetables")
+        //     .attr("font-size","12pt")
+        //     .attr("font-weight", "bold")
+        //     .style("textDecoration","underline");
+
+        // self.chart
+        //     .append("text")
+        //     .attr("x", self.inner_width/2)
+        //     .attr("y", self.inner_height+self.config.margin.bottom/2)
+        //     .attr("text-anchor", "middle")
+        //     .attr("font-size","8pt")
+        //     .attr("font-weight", "bold")
+        //     .text("Vitamine C mg/100g");
     }
 
     update() {
         let self = this;
 
-        const xmin = 0;
-        const xmax = d3.max(self.data, d => d.value);
-        self.xscale.domain([xmin, xmax]);
+        // const xmin = 0;
+        // const xmax = d3.max(self.data, d => d.value);
+        // self.xscale.domain([xmin, xmax]);
 
-        self.yscale.domain(self.data.map(d => d.label));
+        // self.yscale.domain(self.data.map(d => d.label));
 
         self.render();
     }
@@ -98,19 +109,18 @@ class ScatterPlot {
         let self = this;
 
         // Draw bars
-        self.chart.selectAll("rect")
-            .data(self.data)
+        self.chart.selectAll('pie')
+            .data(pie(self.data))
             .enter()
-            .append("rect")
-            .attr("x", 0)
-            .attr("y", d => self.yscale(d.label))
-            .attr("width", d => self.xscale(d.value*1.2))
-            .attr("height", self.yscale.bandwidth());
+            .append('path')
+            .attr('d', self.arc)
+            .attr('fill', 'black')
+            .attr('stroke', 'white')
+            .style('stroke-width', '2px');
+        // self.xaxis_group
+        //     .call(self.xaxis);
 
-        self.xaxis_group
-            .call(self.xaxis);
-
-        self.yaxis_group
-            .call(self.yaxis);
+        // self.yaxis_group
+        //     .call(self.yaxis);
     }
 }
