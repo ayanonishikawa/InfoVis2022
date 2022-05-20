65046efbe09c5cc24d64403f0d51790069fc5473
ayanonishikawa <yaanon1116@gmail.com>
2022-05-20 11:03:03 +0900

    Initial commit

--- W10/task1_main.js
+++ W10/task1_main.js
@@ -6,7 +6,7 @@ d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W04/vitaminC_ranking.csv")
             console.log(d.label + "," + d.value)
         });
         this.data=data;
-        console.log("ok7");
+        console.log("ok8");
     })
     .catch(error => {
         console.log(error);
