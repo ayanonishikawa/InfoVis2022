7960a9a43d63c1ee3badde8e7d511e9adceac680
ayanonishikawa <yaanon1116@gmail.com>
2022-05-20 10:56:07 +0900

    Initial commit

--- W10/task1_main.js
+++ W10/task1_main.js
@@ -1,15 +1,19 @@
 var data;
 d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W04/vitaminC_ranking.csv")
     .then(data => {
-        data.forEach(d => { d.label = d.name; d.value = +d.amount; 
-        console.log(d.label+","+d.value)});
+        data.forEach(d => {
+            d.label = d.name; d.value = +d.amount;
+            console.log(d.label + "," + d.value)
+        });
         console.log("ok7");
     })
     .catch(error => {
         console.log(error);
     });
 
-var margin = {top:10, right:10, bottom:20, left:60};
+var width = 500;
+var height = 300;
+var margin = { top: 50, right: 10, bottom: 70, left: 120};
 
 var svg = d3.select('#drawing_region');
 
@@ -21,31 +25,31 @@ const inner_height = height - margin.top - margin.bottom;
 
 // Initialize axis scales
 const xscale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.value)])
-      .range([0, inner_width]);
+    .domain([0, d3.max(data, d => d.value)])
+    .range([0, inner_width]);
 
 const yscale = d3.scaleBand()
-      .domain(data.map(d => d.label))
-      .range([0, inner_height])
-      .paddingInner(0.1);
+    .domain(data.map(d => d.label))
+    .range([0, inner_height])
+    .paddingInner(0.1);
 
 // Initialize axes
-const xaxis = d3.axisBottom( xscale )
-      .ticks(5)
-      .tickSizeOuter(0);
+const xaxis = d3.axisBottom(xscale)
+    .ticks(5)
+    .tickSizeOuter(0);
 
-const yaxis = d3.axisLeft( yscale )
-      .tickSizeOuter(0);
+const yaxis = d3.axisLeft(yscale)
+    .tickSizeOuter(0);
 
 // Draw the axis
 const xaxis_group = chart.append('g')
-      .attr('transform', `translate(0, ${inner_height})`)
-      .call( xaxis );
+    .attr('transform', `translate(0, ${inner_height})`)
+    .call(xaxis);
 
 const yaxis_group = chart.append('g')
-      .call( yaxis );
+    .call(yaxis);
 
-update( data );
+update(data);
 
 function update(data) {
     svg.selectAll("rect")
@@ -54,7 +58,7 @@ function update(data) {
         .attr("x", 0)
         .attr("y", d => yscale(d.label))
         .attr("width", d => xscale(d.value))
-        .attr("height",yscale.bandwidth());
+        .attr("height", yscale.bandwidth());
 }
 
 d3.select('#reverse')
