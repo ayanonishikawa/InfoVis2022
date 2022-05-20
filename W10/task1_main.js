a9ed8b0342154b02997905d3a624cade343cdba3
ayanonishikawa <yaanon1116@gmail.com>
2022-05-20 10:44:56 +0900

    Initial commit

--- W10/task1_main.js
+++ W10/task1_main.js
@@ -3,25 +3,56 @@ d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W04/vitaminC_ranking.csv")
     .then(data => {
         data.forEach(d => { d.label = d.name; d.value = +d.amount; 
         console.log(d.label+","+d.value)});
-        console.log("ok6");
+        console.log("ok7");
     })
     .catch(error => {
         console.log(error);
     });
 
 var svg = d3.select('#drawing_region');
+
+var chart = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+const inner_width = width - margin.left - margin.right;
+const inner_height = height - margin.top - margin.bottom;
+
+// Initialize axis scales
+const xscale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.value)])
+      .range([0, inner_width]);
+
+const yscale = d3.scaleBand()
+      .domain(data.map(d => d.label))
+      .range([0, inner_height])
+      .paddingInner(0.1);
+
+// Initialize axes
+const xaxis = d3.axisBottom( xscale )
+      .ticks(5)
+      .tickSizeOuter(0);
+
+const yaxis = d3.axisLeft( yscale )
+      .tickSizeOuter(0);
+
+// Draw the axis
+const xaxis_group = chart.append('g')
+      .attr('transform', `translate(0, ${inner_height})`)
+      .call( xaxis );
+
+const yaxis_group = chart.append('g')
+      .call( yaxis );
+
 update( data );
 
 function update(data) {
-    let padding = 10;
-    let height = 20;
     svg.selectAll("rect")
         .data(data)
         .join("rect")
-        .attr("x", padding)
-        .attr("y", (d,i) => padding + i * ( height + padding ))
-        .attr("width", d => d)
-        .attr("height", height);
+        .attr("x", 0)
+        .attr("y", d => yscale(d.label))
+        .attr("width", d => xscale(d.value))
+        .attr("height",yscale.bandwidth());
 }
 
 d3.select('#reverse')
