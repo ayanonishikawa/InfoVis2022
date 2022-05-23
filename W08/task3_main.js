095cec5c0b6481f64ed7b3024e82a53eed63d254
ayanonishikawa <yaanon1116@gmail.com>
2022-05-24 01:14:59 +0900

    Initial commit

--- W08/task3_main.js
+++ W08/task3_main.js
@@ -39,9 +39,6 @@ class PiePlot {
 
         self.chart = self.svg.append('g')
             .attr('transform', `translate(${self.config.width / 2}, ${self.config.height / 2})`);
-        // self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
-        // self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
-        // console.log(self.inner_width + "," + self.inner_height);
 
         self.pie = d3.pie()
             .value(d => d.value);
@@ -53,58 +50,10 @@ class PiePlot {
         self.text = d3.arc()
             .outerRadius(self.radius*0.6)
             .innerRadius(self.radius*0.6);
-
-        // // Initialize axis scales
-        // self.xscale = d3.scaleLinear()
-        //     .range([0, self.inner_width]);
-
-        // self.yscale = d3.scaleBand()
-        //     .range([0, self.inner_height])
-        //     .paddingInner(0.1);
-
-        // // Initialize axes
-        // self.xaxis = d3.axisBottom(self.xscale)
-        //     .ticks(5)
-        //     .tickSizeOuter(0);
-
-        // self.yaxis = d3.axisLeft(self.yscale)
-        //     .tickSizeOuter(0);
-
-        // // Draw the axis
-        // self.xaxis_group = self.chart.append('g')
-        //     .attr('transform', `translate(0, ${self.inner_height})`);
-
-        // self.yaxis_group = self.chart.append('g');
-
-        // self.chart
-        //     .append("text")
-        //     .attr("x", self.config.margin.left)
-        //     .attr("y", -10)
-        //     .attr("text-anchor", "middle")
-        //     .text("Vitamin C content of vegetables")
-        //     .attr("font-size","12pt")
-        //     .attr("font-weight", "bold")
-        //     .style("textDecoration","underline");
-
-        // self.chart
-        //     .append("text")
-        //     .attr("x", self.inner_width/2)
-        //     .attr("y", self.inner_height+self.config.margin.bottom/2)
-        //     .attr("text-anchor", "middle")
-        //     .attr("font-size","8pt")
-        //     .attr("font-weight", "bold")
-        //     .text("Vitamine C mg/100g");
     }
 
     update() {
         let self = this;
-
-        // const xmin = 0;
-        // const xmax = d3.max(self.data, d => d.value);
-        // self.xscale.domain([xmin, xmax]);
-
-        // self.yscale.domain(self.data.map(d => d.label));
-
         self.render();
     }
 
@@ -131,10 +80,5 @@ class PiePlot {
             .attr("font", "7px")
             .attr("text-anchor", "middle")
             .text(function (d) { console.log("label="+d.data.label); return d.data.label; });
-        // self.xaxis_group
-        //     .call(self.xaxis);
-
-        // self.yaxis_group
-        //     .call(self.yaxis);
     }
 }
