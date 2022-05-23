59a4b31c5b48c0dbcb6727ca744b0944242b338f
ayanonishikawa <yaanon1116@gmail.com>
2022-05-23 17:40:35 +0900

    Initial commit

--- W10/task1_main2.js
+++ W10/task1_main2.js
@@ -1,7 +1,7 @@
 d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W04/vitaminC_ranking.csv")
     .then(data => {
         var arrayData=[];
-        console.log("ok14-1");
+        console.log("ok15-1");
         data.forEach(d => {
             d.label = d.name; d.value = +d.amount;
             console.log(d.label + "," + d.value);
@@ -125,18 +125,18 @@ class BarPlot {
 
 d3.select('#reverse')
     .on('click', d => {
-        self.arrayData.reverse();
+        this.arrayData.reverse();
         bar_plot.update();
     });
 
 d3.select('#descend')
     .on('click', d => {
-        self.arrayData.sort((a, b) => b[1] - a[1]);
+        this.arrayData.sort((a, b) => b[1] - a[1]);
         bar_plot.update();
     });
 
 d3.select('#ascend')
     .on('click', d => {
-        self.arrayData.sort((a, b) => a[1] - b[1]);
+        this.arrayData.sort((a, b) => a[1] - b[1]);
         bar_plot.update();
     });
--- W10/task2_maiin.js
+++ W10/task2_maiin.js
@@ -1,82 +1,142 @@
-var data;
-d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W04/vitaminC_ranking.csv")
+d3.csv("https://ayanonishikawa.github.io/InfoVis2022/W08/task2_data.csv")
     .then(data => {
+        var arrayData=[];
+        console.log("ok14-1");
         data.forEach(d => {
-            d.label = d.name; d.value = +d.amount;
-            console.log(d.label + "," + d.value)
+            d.x += d.x; d.y += d.y;
+            console.log(d.x + "," + d.y);
+            arrayData.push([d.x,d.y]);
         });
-        this.data=data;
-        console.log("ok10");
+        console.log("ok11");
+        console.log(arrayData);
+        var config = {
+            parent: '#drawing_region',
+            width: 500,
+            height: 300,
+            margin: { top: 50, right: 10, bottom: 70, left: 120 },
+        };
+        const bar_plot = new ScatterPlot(config, data, arrayData);
+        bar_plot.update();
     })
     .catch(error => {
         console.log(error);
     });
 
-var width = 500;
-var height = 300;
-var margin = { top: 50, right: 10, bottom: 70, left: 120};
-console.log(data.value);
+class ScatterPlot {
+    constructor(config, data, arrayData) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || { top: 10, right: 10, bottom: 10, left: 10 }
+        }
+        this.data = data;
+        this.arrayData=arrayData;
+        console.log(typeof(this.arrayData));
+        this.init();
+    }
 
-var svg = d3.select('#drawing_region');
+    init() {
+        let self = this;
+        self.svg = d3.select(self.config.parent)
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
 
-var chart = svg.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+        console.log(self.inner_width + "," + self.inner_height);
 
-const inner_width = width - margin.left - margin.right;
-const inner_height = height - margin.top - margin.bottom;
+        // Initialize axis scales
+        self.xscale = d3.scaleLinear()
+            .range([0, self.inner_width]);
 
-// Initialize axis scales
-const xscale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.value)])
-    .range([0, inner_width]);
+        self.yscale = d3.scaleBand()
+            .range([0, self.inner_height])
+            .paddingInner(0.1);
 
-const yscale = d3.scaleBand()
-    .domain(data.map(d => d.label))
-    .range([0, inner_height])
-    .paddingInner(0.1);
+        // Initialize axes
+        self.xaxis = d3.axisBottom(self.xscale)
+            .ticks(10)
+            .tickSizeOuter(0);
 
-// Initialize axes
-const xaxis = d3.axisBottom(xscale)
-    .ticks(5)
-    .tickSizeOuter(0);
+        self.yaxis = d3.axisLeft(self.yscale)
+            .tickSizeOuter(0);
 
-const yaxis = d3.axisLeft(yscale)
-    .tickSizeOuter(0);
+        // Draw the axis
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
 
-// Draw the axis
-const xaxis_group = chart.append('g')
-    .attr('transform', `translate(0, ${inner_height})`)
-    .call(xaxis);
+        self.yaxis_group = self.chart.append('g');
 
-const yaxis_group = chart.append('g')
-    .call(yaxis);
+        self.chart
+            .append("text")
+            .attr("x", self.config.margin.left)
+            .attr("y", -20)
+            .attr("text-anchor", "middle")
+            .text("Vitamin C content of vegetables")
+            .attr("font-size", "12pt")
+            .attr("font-weight", "bold")
+            .style("textDecoration", "underline");
 
-update(data);
+        self.chart
+            .append("text")
+            .attr("x", self.inner_width / 2)
+            .attr("y", self.inner_height + self.config.margin.bottom / 2)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "8pt")
+            .attr("font-weight", "bold")
+            .text("Vitamine C mg/100g");
+    }
 
-function update(data) {
-    svg.selectAll("rect")
-        .data(data)
-        .join("rect")
-        .attr("x", 0)
-        .attr("y", d => yscale(d.label))
-        .attr("width", d => xscale(d.value))
-        .attr("height", yscale.bandwidth());
+    update() {
+        let self = this;
+
+        const xmin = 0;
+        const xmax = d3.max(self.arrayData, d => d[1]);
+        self.xscale.domain([xmin, xmax]);
+
+        self.yscale.domain(self.arrayData.map(d => d[0]));
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        // Draw bars
+        self.chart.selectAll("circle")
+            .data(self.arrayData)
+            .enter()
+            .append("circle")
+            .attr("x", 0)
+            .attr("y", d => self.yscale(d[0]))
+            .attr("width", d => self.xscale(d[1]))
+            .attr("height", self.yscale.bandwidth());
+
+        self.xaxis_group
+            .call(self.xaxis);
+
+        self.yaxis_group
+            .call(self.yaxis);
+    }
 }
 
 d3.select('#reverse')
     .on('click', d => {
-        data.reverse();
-        update(data);
+        self.arrayData.reverse();
+        bar_plot.update();
     });
 
 d3.select('#descend')
     .on('click', d => {
-        data.reverse();
-        update(data);
+        self.arrayData.sort((a, b) => b[1] - a[1]);
+        bar_plot.update();
     });
 
 d3.select('#ascend')
     .on('click', d => {
-        data.reverse();
-        update(data);
+        self.arrayData.sort((a, b) => a[1] - b[1]);
+        bar_plot.update();
     });
